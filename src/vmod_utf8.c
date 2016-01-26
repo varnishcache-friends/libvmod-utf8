f162535ccb005b8540e83f56604a2bd374ce52c0
Federico G. Schwindt <fgsch@lodoss.net>
2016-01-26 18:57:46 +0000

    Remove unneeded parameters, libs and includes

--- src/vmod_utf8.c
+++ src/vmod_utf8.c
@@ -26,14 +26,11 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdlib.h>
-
 #include <utf8proc.h>
 
 #include "cache/cache.h"
 
 #include "vrt.h"
-#include "vsa.h"
 
 #include "vcc_if.h"
 
